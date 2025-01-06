c0fedbea14af1d043e8b8d31d5f737ac590824a3
tfiorin <tfiorin@gmail.com>
2025-01-05 19:34:24 -0700

    3. Testing Contracts

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,8 +4,14 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract Token {    
-    string public name = "My Token";
-    string public symbol = "MYT";
-    uint256 public totalSupply = 1000000;
-    uint8 public decimals = 18;
+    string public name;
+    string public symbol;
+    uint256 public totalSupply;
+    uint256 public decimals = 18;
+
+    constructor(string memory _name, string memory _symbol, uint256 _totalSupply) {
+        name = _name;
+        symbol = _symbol;
+        totalSupply = _totalSupply * 10 ** 18;
+    }
 }
