61f9de8ede1e325411218dd4df91ece4ab0e3852
tfiorin <tfiorin@gmail.com>
2025-01-15 19:14:58 -0700

    6. Token Approvals

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -9,9 +9,38 @@ contract Token {
     uint256 public totalSupply;
     uint256 public decimals = 18;
 
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+
     constructor(string memory _name, string memory _symbol, uint256 _totalSupply) {
         name = _name;
         symbol = _symbol;
         totalSupply = _totalSupply * 10 ** 18;
+        balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value, "Insufficient balance");
+        require(_to != address(0), "Invalid address");
+
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        
+        emit Transfer(msg.sender, _to, _value);
+        
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        require(_spender != address(0), "Invalid address");
+
+        allowance[msg.sender][_spender] = _value;
+
+        emit Approval(msg.sender, _spender, _value);
+
+        return true;
     }
 }
--- test/Token.t.js
+++ test/Token.t.js
@@ -6,11 +6,16 @@ const tokens = (n) => {
 }
 
 describe("Token", () => {  
-    let token;
+    let token, accounts, deployer, receiver, exchange;
 
     beforeEach(async () => {
         const Token = await ethers.getContractFactory("Token");
         token = await Token.deploy("Dapp University","DAPP", 1000000);
+
+        accounts = await ethers.getSigners();
+        deployer = accounts[0];
+        receiver = accounts[1];
+        exchange = accounts[2];
     });
 
     describe("Deployment", () => {
@@ -34,6 +39,71 @@ describe("Token", () => {
         it("has correct total supply", async () => {
             expect(await token.totalSupply()).to.equal(totalSupply);
         });
+
+        it("assigns total supply to deployer", async () => {
+            expect(await token.balanceOf(deployer.address)).to.equal(totalSupply);
+        });
     });   
     
+    describe("Sending Tokens", () => {
+        let amount, transaction, result;
+
+        describe("Success", () => {
+            beforeEach(async () => {
+                amount = tokens(100);
+                transaction = await token.connect(deployer).transfer(receiver.address, amount);
+                result = transaction.wait();
+            });
+    
+            it("transfers token balance", async () => {
+                expect(await token.balanceOf(deployer.address)).to.equal(tokens(999900));
+                expect(await token.balanceOf(receiver.address)).to.equal(amount);
+            });
+    
+            it("emits Transfer event", async () => {
+                expect(transaction).to.emit(token, "Transfer").withArgs(deployer.address, receiver.address, amount);
+            });
+        });
+
+        describe("Failure", () => {
+            it("rejects insufficient balance", async () => {
+                amount = tokens(1000001);
+                await expect(token.connect(deployer).transfer(receiver.address, amount)).to.be.revertedWith("Insufficient balance");
+            });
+    
+            it("rejects invalid recipient", async () => {
+                amount = tokens(100);
+                await expect(token.connect(deployer).transfer(ethers.constants.AddressZero, amount)).to.be.revertedWith("Invalid address");
+            });
+        });
+    });
+
+    describe("Approving Tokens", () => {
+        let amount, transaction, result;
+
+        beforeEach(async () => {
+            amount = tokens(100);
+            transaction = await token.connect(deployer).approve(exchange.address, amount);
+            result = transaction.wait();
+        });
+
+        describe("Success", () => {           
+    
+            it("approves token spending", async () => {
+                expect(await token.allowance(deployer.address, exchange.address)).to.equal(amount);
+            });
+    
+            it("emits Approval event", async () => {
+                expect(transaction).to.emit(token, "Approval").withArgs(deployer.address, exchange.address, amount);
+            });
+        });
+
+        describe("Failure", () => {
+            it("rejects invalid spender", async () => {
+                amount = tokens(100);
+                await expect(token.connect(deployer).approve(ethers.constants.AddressZero, amount)).to.be.revertedWith("Invalid address");
+            });
+        });
+        
+    });
 });
