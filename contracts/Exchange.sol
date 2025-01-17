8091cec782a42b11c12e8d5ad5e8a8e5490a1598
tfiorin <tfiorin@gmail.com>
2025-01-17 12:27:43 -0700

    8. Create Exchange contract

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -1,14 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "hardhat/console.sol";
+import { console } from "hardhat/console.sol";
+import { Token } from "./Token.sol";
 
 contract Exchange { 
     address public feeAccount; // the account that receives exchange fees
     uint256 public feePercent; // the fee percentage
+    mapping(address => mapping(address => uint256)) public tokens;
+
+    event Deposit(address token, address user, uint256 amount, uint256 balance);
 
     constructor(address _feeAccount, uint256 _feePercent) {
         feeAccount = _feeAccount;
         feePercent = _feePercent;
+    }
+
+    function deposit(address _token, uint256 _amount) public {
+        // transfer token to exchange
+        Token(_token).transferFrom(msg.sender, address(this), _amount);
+        // update balance
+        tokens[_token][msg.sender] += _amount;
+        // emit event
+        emit Deposit(_token, msg.sender, _amount, tokens[_token][msg.sender]);
+    }
+
+    function checkBalance(address _token, address _user) public view returns (uint256) {
+        return tokens[_token][_user];
     }
 }
--- test/Exchange.t.js
+++ test/Exchange.t.js
@@ -6,7 +6,7 @@ const tokens = (n) => {
 }
 
 describe("Exchange", () => {  
-    let exchange, deployer, feeAccount;
+    let exchange, deployer, feeAccount, token1, user1;
 
     const feePercent = 10;
 
@@ -14,8 +14,16 @@ describe("Exchange", () => {
         let accounts = await ethers.getSigners();
         deployer = accounts[0];
         feeAccount = accounts[1];
+        user1 = accounts[2];
 
+        const Token = await ethers.getContractFactory("Token");
         const Exchange = await ethers.getContractFactory("Exchange");
+
+        token1 = await Token.deploy("Dapp University","DAPP", 1000000);
+
+        let transaction = await token1.connect(deployer).transfer(user1.address, tokens(100));
+        await transaction.wait();
+        
         exchange = await Exchange.deploy(feeAccount.address, feePercent);
     });
 
@@ -26,6 +34,47 @@ describe("Exchange", () => {
 
         it("tracks the fee percent", async () => {
             expect(await exchange.feePercent()).to.equal(feePercent);
+        });
+    });
+    
+    describe("Depositing Tokens", () => {
+        describe("Success", () => {
+            let amount, transaction, result;
+
+            beforeEach(async () => {
+                amount = tokens(10);
+
+                //Approve the exchange to spend the tokens
+                transaction = await exchange.connect(user1).depositToken("DAPP", amount);
+                result = await transaction.wait();
+
+                //Deposit tokens
+                transaction = await token1.connect(user1).approve(exchange.address, amount);
+                result = await transaction.wait();
+            });
+    
+            it("tracks the token deposit", async () => {
+                expect(await token1.balanceOf(exchange.address)).to.equal(amount);
+                expect(await exchange.tokens(token1.address, user1.address)).to.equal(amount);
+                expect(await exchange.balanceOf(token1.address, user1.address)).to.equal(amount);
+            });
+
+            it('emits a Deposit event', async () => {
+                const event = result.events[1]; // 2 events are emitted
+                expect(event.event).to.equal('Deposit');
+        
+                const args = event.args;
+                expect(args.token).to.equal(token1.address);
+                expect(args.user).to.equal(user1.address);
+                expect(args.amount).to.equal(amount);
+                expect(args.balance).to.equal(amount);
+              })
+        });
+
+        describe("Failure", () => {
+            it("rejects invalid token", async () => {
+                await expect(exchange.connect(user1).depositToken(token1.address, tokens(100))).to.be.revertedWith("invalid token");
+            });
         });
     });
 });
