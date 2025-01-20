8ef3641eaa5c64b4a084dca2f811ce53cbe844ca
tfiorin <tfiorin@gmail.com>
2025-01-19 17:22:14 -0700

    11. Make orders

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -8,14 +8,31 @@ contract Exchange {
     address public feeAccount; // the account that receives exchange fees
     uint256 public feePercent; // the fee percentage
     mapping(address => mapping(address => uint256)) public tokens;
+    mapping(uint256 => _Order) public orders;
+    uint256 public orderCount;
 
     event Deposit(address token, address user, uint256 amount, uint256 balance);
     event Withdraw(address token, address user, uint256 amount, uint256 balance);
+    event Order(uint256 id, address user, address tokenGet, uint256 amountGet, address tokenGive, uint256 amountGive, uint256 timestamp);
+
+    struct _Order {
+        uint256 id;
+        address user;
+        address tokenGet;
+        uint256 amountGet;
+        address tokenGive;
+        uint256 amountGive;
+        uint256 timestamp;
+    }
 
     constructor(address _feeAccount, uint256 _feePercent) {
         feeAccount = _feeAccount;
         feePercent = _feePercent;
     }
+
+    // -----------------------------------------
+    // DEPOSIT & WITHDRAW TOKEN
+    // -----------------------------------------
 
     function depositToken(address _token, uint256 _amount) public {
         // transfer token to exchange
@@ -39,5 +56,27 @@ contract Exchange {
 
     function balanceOf(address _token, address _user) public view returns (uint256) {
         return tokens[_token][_user];
+    }
+
+    // -----------------------------------------
+    // MAKE & CANCEL ORDERS
+    // -----------------------------------------
+
+    function makeOrder(address _tokenGet, uint256 _amountGet, address _tokenGive, uint256 _amountGive) public {
+        require(balanceOf(_tokenGive, msg.sender) >= _amountGive, "Insufficient balance");
+
+        orderCount += 1;
+        // instantiate a new order
+        orders[orderCount] = _Order(
+            orderCount,
+            msg.sender,
+            _tokenGet,
+            _amountGet,
+            _tokenGive,
+            _amountGive,
+            block.timestamp
+        );
+        // emit event
+        emit Order(orderCount, msg.sender, _tokenGet, _amountGet, _tokenGive, _amountGive, block.timestamp);
     }
 }
--- test/Exchange.t.js
+++ test/Exchange.t.js
@@ -6,7 +6,7 @@ const tokens = (n) => {
 }
 
 describe("Exchange", () => {  
-    let exchange, deployer, feeAccount, token1, user1;
+    let exchange, deployer, feeAccount, token1, user1, token2;
 
     const feePercent = 10;
 
@@ -20,6 +20,7 @@ describe("Exchange", () => {
         const Exchange = await ethers.getContractFactory("Exchange");
 
         token1 = await Token.deploy("Dapp University","DAPP", 1000000);
+        token2 = await Token.deploy("Mock Dai","MDAI", 1000000);
 
         let transaction = await token1.connect(deployer).transfer(user1.address, tokens(100));
         await transaction.wait();
@@ -140,6 +141,53 @@ describe("Exchange", () => {
 
         it("returns user balance", async () => {
             expect(await exchange.balanceOf(token1.address, user1.address)).to.equal(amount);
+        });
+    });
+
+    describe("Making Orders", () => {
+        let amount, transaction, result;
+        amount = tokens(1);
+
+        describe("Success", () => {
+            let orderId, price;
+
+            beforeEach(async () => {
+                //Approve the exchange to spend the tokens
+                transaction = await token1.connect(user1).approve(exchange.address, amount);
+                result = await transaction.wait();   
+
+                //Deposit tokens
+                transaction = await exchange.connect(user1).depositToken(token1.address, amount);
+                result = await transaction.wait();
+                
+                //Make order
+                transaction = await exchange.connect(user1).makeOrder(token2.address, amount, token1.address, amount);
+                result = await transaction.wait();
+            });
+
+            it("tracks the order", async () => {
+                expect(await exchange.orderCount()).to.equal(1);
+            });
+
+            it('emits an Order event', async () => {
+                const event = result.events[0]; // 2 events are emitted
+                expect(event.event).to.equal('Order');
+        
+                const args = event.args;
+                expect(args.id).to.equal(1);
+                expect(args.tokenGet).to.equal(token2.address);
+                expect(args.amountGet).to.equal(amount);
+                expect(args.tokenGive).to.equal(token1.address);
+                expect(args.amountGive).to.equal(amount);
+                expect(args.user).to.equal(user1.address);
+                expect(args.timestamp).to.exist;
+              })
+        });
+
+        describe("Failure", () => {
+            it("rejects invalid token", async () => {
+                await expect(exchange.connect(user1).makeOrder(token2.address, amount, token1.address, amount)).to.be.reverted;
+            });
         });
     });
 });
